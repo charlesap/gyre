90b6e4b4310c007d97770f2870b5560354e67551
Charles Perkins <charlesap@gmail.com>
2025-01-23 10:25:56 -0800

    catch up

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./gyre.py gyr/creature.gyr gyr/motor.gyr gyr/sensory.gyr gyr/brain.gyr
-pushd .; cd c; gcc -shared -o cortical.so cortical.c; popd
-mpicc -o creature creature.c c/cortical.c c/interface.c
-mpirun -np 8 ./creature
+pushd .; cd c; gcc -shared -o cortical.so cortical.c; gcc -shared -o interface.so interface.c; popd
+mpicc -o gyre c/gyre.c c/cortical.c c/interface.c
+mpirun -np 8 ./gyre -d ./creature.def -m 1024
 

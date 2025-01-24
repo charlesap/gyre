3181cbc5fb6700a3e3970661281a30fb84de9b93
Charles Perkins <charlesap@gmail.com>
2025-01-24 11:39:35 -0800

    update build-and-run script and hints

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ./gyre.py gyr/creature.gyr gyr/motor.gyr gyr/sensory.gyr gyr/brain.gyr
-pushd .; rm gyre; cd c; make shared; make; mv gyre ../; popd
+pushd .; rm gyre; cd c; make clean; make shared; make; mv gyre ../; popd
 mpirun -np 8 ./gyre -d ./creature.def -m 1024
 

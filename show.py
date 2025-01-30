d46afbcc669fbf6ba4389183ce3e791b15d772ca
Charles Perkins <charlesap@gmail.com>
2025-01-30 15:09:59 -0800

    place content in tabs

--- show.py
+++ show.py
@@ -7,19 +7,34 @@ from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QDialog, QPlainTextEdit, QPushButton, QVBoxLayout
 
-class Window(QMainWindow):
-    """Main Window."""
-    def __init__(self, parent=None):
-        """Initializer."""
-        super().__init__(parent)
-        self.setWindowTitle("Gyre Simulation")
-        self.resize(600, 200)
-        self.centralWidget = QLabel("Gyre")
-        self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        self.setCentralWidget(self.centralWidget)
+from PyQt5.QtWidgets import QWidget, QAction, QTabWidget
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
 
-    def update(self,text):
-        self.centralWidget = QLabel(text)
+class Sim:
+    cores = 0
+    hosts = None
+    pids = None
+
+sim = Sim()
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'Gyre'
+        self.left = 0
+        self.top = 0
+        self.width = 800
+        self.height = 600
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        self.table_widget = MyTableWidget(self)
+        self.setCentralWidget(self.table_widget)
+        
+        self.show()
+
 
 class QTextEditLogger(logging.Handler):
     def __init__(self, parent):
@@ -43,38 +58,58 @@ class StdinReader(QThread):
             self.inputReceived.emit(line.strip())
 
 
+class MyTableWidget(QWidget):
 
-class MyDialog(QDialog, QPlainTextEdit):
-    def __init__(self, win, parent=None):
-        super().__init__(parent)
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
 
-        self.topwin = win
-        self.resize(800, 400)
-        self.setWindowTitle("Gyre Log")
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tabOVR = QWidget()
+        self.tabDTL = QWidget()
+        self.tabDGN = QWidget()
+        self.tabLOG = QWidget()
+        self.tabs.resize(800,600)
+
+        # Add tabs
+        self.tabs.addTab(self.tabOVR,"Overview")
+        self.tabs.addTab(self.tabDTL,"Detail")
+        self.tabs.addTab(self.tabDGN,"Diagnostics")
+        self.tabs.addTab(self.tabLOG,"Log")
+
+        # Create Overview tab
+        self.tabOVR.layout = QVBoxLayout(self)
+        self.pushButton1 = QPushButton("Push Me")
+        self.tabOVR.layout.addWidget(self.pushButton1)
+        self.tabOVR.setLayout(self.tabOVR.layout)
+
+        # Create Log tab
+        self.tabLOG.layout = QVBoxLayout(self)
+        self.plainTextEditLog = QPlainTextEdit()
+        self.tabLOG.layout.addWidget(self.plainTextEditLog)
+        self.tabLOG.setLayout(self.tabLOG.layout)
 
         self.stdin_reader = StdinReader()
-        self.stdin_reader.inputReceived.connect(self.append)
+        self.stdin_reader.inputReceived.connect(self.logAppend)
         self.stdin_reader.start()
 
-        logTextBox = QTextEditLogger(self)
-#        logTextBox.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        logTextBox = QTextEditLogger(self.plainTextEditLog)
         logging.getLogger().addHandler(logTextBox)
         logging.getLogger().setLevel(logging.DEBUG)
 
-#        self._button = QPushButton(self)
-#        self._button.setText('Test Me')
 
-        layout = QVBoxLayout()
-        # Add the new logging box widget to the layout
-        layout.addWidget(logTextBox.widget)
-#        layout.addWidget(self._button)
-        self.setLayout(layout)
+#        self.tabLOG.layout = QVBoxLayout(self)
+        self.tabLOG.layout.addWidget(logTextBox.widget)
+        self.setLayout(self.tabLOG.layout)
 
-        # Connect signal to slot
-#        self._button.clicked.connect(self.test)
+        # Add tabs to widget
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
 
-    def append(self, text):
-        logging.debug(text)
+
+    def logAppend(self, text):
+        logging.debug(text) # logging.debug logging.info logging.warning logging.error
         parts = text.split(',')
         if len(parts)>3:
             first = parts[0].split(':')
@@ -83,28 +118,29 @@ class MyDialog(QDialog, QPlainTextEdit):
                     pid = parts[1].split(':')
                     rank = parts[2].split(':')
                     world = parts[3].split(':')
-                    win.setWindowTitle(first[1])
-                    try:
-                        hosts
-                    except NameError:
-                        hosts = [0] * int(world[1])
-                        pids = [0] * int(world[1])
-                    hosts[int(rank[1])]=first[1]
-                    pids[int(rank[1])]=pid[1]
+                    #self.topwin.setWindowTitle(first[1])
+                    global sim
+                    #global pids
+                    if sim.hosts == None:
+                        sim.hosts = [0] * int(world[1])
+                        sim.pids = [0] * int(world[1])
+                    sim.cores=int(rank[1])
+                    sim.hosts[int(rank[1])]=first[1]
+                    sim.pids[int(rank[1])]=pid[1]
 
-#    def test(self):
-#        logging.debug('damn, a bug')
-#        logging.info('something to remember')
-#        logging.warning('that\'s not right')
-#        logging.error('foobar')
+
+    @pyqtSlot()
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+
+
 
 if __name__ == "__main__":
+    sim = Sim()
     app = QApplication(sys.argv)
-    win = Window()
-    dlg = MyDialog(win)
-    dlg.show()
-    dlg.raise_()
-    win.show()
+    ex=App()
 
 
     sys.exit(app.exec_())

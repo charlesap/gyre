66cc2eac9aa0b5864697e3b0f1ad1671dff57c74
Charles Perkins <charlesap@gmail.com>
2025-01-31 13:56:05 -0800

    report initializing state to log

--- c/gyre.c
+++ c/gyre.c
@@ -88,6 +88,10 @@ int main(int argc, char** argv) {
 
 
       printf("hostname:%s,pid:%d,rank:%d,world:%d\n",hname, getpid(), rank, world);fflush(stdout);
+      MPI_Barrier(MPI_COMM_WORLD);
+      printf("INITIALIZING:%d\n",rank);fflush(stdout);
+      MPI_Barrier(MPI_COMM_WORLD);
+
       for(int i=0;(done==0)&&(i<60);i++){
         MPI_Barrier(MPI_COMM_WORLD);
         done = communicate(0,rank,world,v,r);
